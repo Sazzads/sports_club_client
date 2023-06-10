82933683ff344d5112c3fa12195ff59dcc018ea4
Sazzads <sazzadtk@gmail.com>
2023-06-10 17:11:11 +0600

    fetch data in approved class page

--- src/Pages/Home/AllClasses/AllClasses.jsx
+++ src/Pages/Home/AllClasses/AllClasses.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
+
 
 const AllClasses = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/approvedclass/approved')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setData(data)
+            })
+    }, [])
 
     return (
         <div>
-            
+            {data.length}
         </div>
     );
 };
